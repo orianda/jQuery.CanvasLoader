fce1815c142e84dfbfd53d79076ee872c6fcadd6
Orianda <orianda@paan.de>
2014-08-05 18:13:20 +0200

    added promise handling

--- src/index.js
+++ src/index.js
@@ -13,108 +13,194 @@
     }());
 
     /**
+     * Is value an valid one?
+     * @param {*} value
+     * @returns {boolean}
+     */
+    function isValid(value) {
+        return !(typeof value === 'undefined' || value === null || typeof value === 'number' && isNaN(value));
+    }
+
+    /**
+     * Is the value an object?
+     * @param {*} value
+     * @returns {boolean}
+     */
+    function isObject(value) {
+        return typeof value === 'object' && value !== null;
+    }
+
+    /**
+     * Is value an promise object?
+     * @param {*} value
+     * @returns {boolean}
+     */
+    function isPromise(value) {
+        return isObject(value) && typeof value.then === 'function';
+    }
+
+    /**
+     * Get option of an options set
+     * @param {string} name
+     * @param {Array} configs
+     * @param {jQuery} element
+     * @returns {*}
+     */
+    function getOption(name, configs, element) {
+        var config, value, i, l;
+        for (i = 0, l = configs.length; i < l; i++) {
+            config = isObject(configs[i]) ? configs[i] : $.fn.canvasLoader.options[configs[i]];
+            if (config) {
+                value = typeof config[name] === 'function' ? config[name](element) : config[name];
+                if (isValid(value)) {
+                    window.console && window.console.log('success');
+                    return value;
+                }
+            }
+        }
+        return undefined;
+    }
+
+    /**
      * Adds an CanvasLoader overlay to the element
-     * @param {Object} [options]
-     * @param {string} [options.shape]
-     * @param {string} [options.color]
-     * @param {number} [options.diameter]
-     * @param {number} [options.density]
-     * @param {number} [options.range]
-     * @param {number} [options.speed]
-     * @param {number} [options.fps]
+     * This module takes multiple options arguments. The first is the major one,
+     * if it misses needed properties, then the next options will be searched for.
+     * If the next also misses the required property, then the third will be used and so on.
+     *
+     * @param {Object|string} [options] Options object or composition name
+     * @param {boolean|promise} [options.start] Set to false to prevent autostart or provide an promise object,
+     * to automatically stop the pending when the promise resolves or rejects.
+     * @param {Function|string} [options.shape]
+     * @param {Function|string} [options.color]
+     * @param {Function|number} [options.diameter]
+     * @param {Function|number} [options.density]
+     * @param {Function|number} [options.range]
+     * @param {Function|number} [options.speed]
+     * @param {Function|number} [options.fps]
+     * @param {Object|string} [fallback1] See option argument
+     * @param {Object|string} [fallback2] See option argument
      * @return {jQuery}
      */
-    $.fn.canvasLoader = function (options) {
+    $.fn.canvasLoader = function (options, fallback1, fallback2) {
         var collection = this;
 
+        if (!window.CanvasLoader) {
+            if (window.console) {
+                window.console.error('CanvasLoader library missing!');
+            }
+            return collection;
+        }
+
         /**
-         * Extend given options by default ones
-         * @type {Object}
+         * Destroy existing canvas loader setups
          */
-        options = $.extend({}, $.fn.canvasLoader.options, options);
+        collection.trigger('destroy.canvasLoader');
 
         /**
          * Setup loader for each element
          */
-        this.each(function () {
+        collection.each(function () {
             var capsule = $(this),
                 position = capsule.css('position'),
                 overlay,
                 animator,
                 id = 'canvasLoader' + uid();
 
-            if (!window.CanvasLoader) {
-                if (window.console) {
-                    window.console.error('CanvasLoader Library missing!');
+            /**
+             * Start canvas loader by event
+             */
+            capsule.on('start.canvasLoader', function () {
+                if (capsule.find('> .canvasLoader').length) {
+                    return;
                 }
-                return;
-            }
 
-            if (capsule.find('> .canvasLoader').length) {
-                return;
-            }
+                if (position === 'static') {
+                    capsule.css('position', 'relative');
+                }
 
-            if (position === 'static') {
-                capsule.css('position', 'relative');
-            }
+                overlay = $('<div id="' + id + '" class="canvasLoader"></div>').appendTo(capsule);
 
-            overlay = $('<div id="' + id + '" class="canvasLoader"></div>').appendTo(capsule);
+                window.console && window.console.log('get color', getOption('color', collection.canvasLoader.options, capsule));
+                animator = new window.CanvasLoader(id);
+                animator.setShape(getOption('shape', collection.canvasLoader.options, capsule));
+                animator.setColor(getOption('color', collection.canvasLoader.options, capsule));
+                animator.setDiameter(getOption('diameter', collection.canvasLoader.options, capsule));
+                animator.setDensity(getOption('density', collection.canvasLoader.options, capsule));
+                animator.setRange(getOption('range', collection.canvasLoader.options, capsule));
+                animator.setSpeed(getOption('speed', collection.canvasLoader.options, capsule));
+                animator.setFPS(getOption('fps', collection.canvasLoader.options, capsule));
+                animator.show();
 
-            animator = new window.CanvasLoader(id);
-            animator.setShape(options.shape);
-            animator.setColor(options.color);
-            animator.setDiameter(options.diameter);
-            animator.setDensity(options.density);
-            animator.setRange(options.range);
-            animator.setSpeed(options.speed);
-            animator.setFPS(options.fps);
-            animator.show();
-
-            overlay.find('> div').css({
-                marginLeft: options.diameter / -2,
-                marginTop: options.diameter / -2
+                overlay.find('> div').css({
+                    marginLeft: animator.getDiameter() / -2,
+                    marginTop: animator.getDiameter() / -2
+                });
             });
 
             /**
              * Stop canvas loader by event
              */
-            capsule.one('stop.canvasLoader', function () {
-                animator.kill();
-                overlay.remove();
-                capsule.css({position: position});
+            capsule.on('stop.canvasLoader', function () {
+                if (overlay && animator) {
+                    animator.kill();
+                    overlay.remove();
+                    capsule.css({position: position});
+                    overlay = animator = null;
+                }
+            });
+
+            /**
+             * Cleanup canvas loader instance
+             */
+            capsule.on('destroy.canvasLoader', function () {
+                capsule.trigger('stop.canvasLoader');
+                capsule.off('.canvasLoader');
             });
         });
 
         /**
-         * Reinitialize canvas loader by event
-         */
-        this.on('start.canvasLoader', function () {
-            $(collection).canvasLoader(options);
-        });
-
-        /**
-         * Switching canvas loader on or off
-         * @param {boolean} on
+         * Overwrite module function in order to change the habits
+         * @param {boolean|promise} on
          * @returns {jQuery}
          */
-        this.canvasLoader = function (on) {
-            return collection.trigger((on ? 'start' : 'stop') + '.canvasLoader');
+        collection.canvasLoader = function (on) {
+            if (isPromise(on)) {
+                collection.canvasLoader(true);
+                on.then(function () {
+                    collection.canvasLoader(false);
+                }, function () {
+                    collection.canvasLoader(false);
+                });
+            } else {
+                collection.trigger((on ? 'start' : 'stop') + '.canvasLoader');
+            }
+            return collection;
         };
 
         /**
          * Make options available for manipulations
-         * @type {Object}
          */
-        this.canvasLoader.options = options;
+        collection.canvasLoader.options = Array.prototype.slice.call(arguments);
+        collection.canvasLoader.options.push($.fn.canvasLoader.options.defaults);
 
-        return this;
+        /**
+         * Initialize canvas loader
+         */
+        return collection.canvasLoader(getOption('start', collection.canvasLoader.options, collection));
     };
+
+    /**
+     * Namespace for custom defined options compositions
+     * @type {Object}
+     */
+    $.fn.canvasLoader.options = {};
 
     /**
      * Modules default options
      * @type {Object}
      */
-    $.fn.canvasLoader.options = {
+    $.fn.canvasLoader.options.defaults = {
+        start: true,
         shape: 'spiral',
         color: '#000',
         diameter: 70,
@@ -122,6 +208,62 @@
         range: 0.7,
         speed: 2,
         fps: 24
+    };
+
+    /**
+     * Options to retrieve the configuration from css properties
+     * @type {Object}
+     */
+    $.fn.canvasLoader.options.css = {
+        shape: function (element) {
+            return element.css('font-family');
+        },
+        color: function (element) {
+            return element.css('color');
+        },
+        diameter: function (element) {
+            return parseFloat(element.css('line-height'));
+        },
+        density: function (element) {
+            return parseFloat(element.css('letter-spacing'));
+        },
+        range: function (element) {
+            return parseFloat(element.css('font-weight'));
+        },
+        speed: function (element) {
+            return parseFloat(element.css('font-size'));
+        },
+        fps: function (element) {
+            return parseFloat(element.css('text-indent'));
+        }
+    };
+
+    /**
+     * Options to retrieve the configuration from data attributes of the element
+     * @type {Object}
+     */
+    $.fn.canvasLoader.options.data = {
+        shape: function (element) {
+            return element.data('canvas-loader-shape');
+        },
+        color: function (element) {
+            return element.data('canvas-loader-color');
+        },
+        diameter: function (element) {
+            return parseFloat(element.data('canvas-loader-diameter'));
+        },
+        density: function (element) {
+            return parseFloat(element.data('canvas-loader-density'));
+        },
+        range: function (element) {
+            return parseFloat(element.data('canvas-loader-range'));
+        },
+        speed: function (element) {
+            return parseFloat(element.data('canvas-loader-speed'));
+        },
+        fps: function (element) {
+            return parseFloat(element.data('canvas-loader-fps'));
+        }
     };
 
     /**
